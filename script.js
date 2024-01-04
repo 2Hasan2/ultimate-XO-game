e64b75e30bc2ad6b6bfb0259ac9d218d2f1daee9
2Hasan2 <h01554176846@gmail.com>
2024-01-04 23:06:05 +0200

    add render function

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+const container = document.querySelector('.box-container')
+const boxes = document.querySelectorAll('.box');
 const arrays = [];
+
+
+Resize();
+function Resize(){
+    const size = Math.min(window.innerWidth, window.innerHeight);
+    container.style.width = size + 'px';
+    container.style.height = size + 'px';
+}
+window.addEventListener('resize', () => {Resize()})
+
+
+boxes.forEach((box,index) => {
+    box.id = index;
+   for(let i = 0; i < 9; i++){
+        const div = document.createElement('div');
+        div.classList.add('XO-box');
+        div.dataset.row = Math.floor(i / 3);
+        div.dataset.col = i % 3;
+        box.appendChild(div);
+        div.addEventListener('click', (e) => {
+           play(e, index)
+        })
+   }
+})
 
 for (let i = 0; i < 9; i++) {
     const array = [
@@ -20,18 +46,20 @@ function play(event,board_index) {
     if (board_index !== board) return;
     if (event.target.innerText !== "") return;
     const { row, col } = event.target.dataset;
-    event.target.innerText = player;
-    console.log(board, +row, +col);
     arrays[board][+row][+col] = player;
     console.log(arrays[board]);
     board = +row * 3 + +col;
+    render();
     switchPlayer();
 }
 
-// function render() {
-//     console.log(arrays[board]);
-//     boxes.forEach((index, box) => {
-//         const [row, col] = index.split("-");
-//         box.innerText = arrays[board][row][col];
-//     });
-// }
+function render() {
+    boxes.forEach((box, index) => {
+        box = [...box.children];
+        box.forEach((cell) => {
+            const { row, col } = cell.dataset;
+            cell.innerText = arrays[index][+row][+col];
+        });
+    });
+
+}
